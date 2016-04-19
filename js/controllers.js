da0409104ca54d046969e2bee0aab7d70b257f30
Koen Creve <koen@koencreve.local>
2016-04-19 16:32:41 +0200

    Facebook login: BEZIG

--- js/controllers.js
+++ js/controllers.js
@@ -18,11 +18,75 @@ sitababyApp.config(['$routeProvider',
                 controller: "contactCtrl"
             })
 }]);
+//INDEX CONTROLLER
+sitababyApp.controller('indexCtrl', ['$scope',
+    function ($scope){
+        
+        // facebook login sdk
+        function statusChangeCallback(response) {
+            console.log('statusChangeCallback');
+            console.log(response);
+            if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            testAPI();
+            } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+            document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+            } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+            document.getElementById('status').innerHTML = 'Please log ' +
+            'into Facebook.';
+            }
+        }
+
+        // This function is called when someone finishes with the Login
+        // Button.  See the onlogin handler attached to it in the sample
+        // code below.
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+            });
+        }
+
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '986227158099684',
+                cookie     : true, // enables cookies to allow the server to access the session
+                xfbml      : true, //parse social plugins on this page
+                version    : 'v2.6' // use graph api version 2.5
+            });
+
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });    
+        };
+        
+        //Load the SDK asynchronously 
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        function testAPI() {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function(response) {
+                console.log('Successful login for: ' + response.name);
+                document.getElementById('status').innerHTML =
+                    'Thanks for logging in, ' + response.name + '!';
+            });
+        }
+    }
+]);
 
 //HOME CONTROLLER
 sitababyApp.controller('homeCtrl', ['$scope',
-	function ($scope) {
-
+    function ($scope) {
+        
 	}
 ]);
 
@@ -43,30 +107,6 @@ sitababyApp.controller('contactCtrl', ['$scope',
 
 	}
 ]);
-/*
-//FB LOGIN CONTROLLER
-sitababyApp.controller("facebookLogin", function ($scope) {
-    $scope.getData = function () {
-        var deferred = $scope.defer();
-        FB.api('/me', (
-            fields: 'last_name'
-        ), function (response) {
-            if (!response || response.error) {
-                deferred.reject('Error occured!');
-            } else {
-                deferred.resolve(response);
-            }
-        });
-        return deferred.promise;
-    }
-    $scope.getMyLastName = function () {
-        facebookLogin.getData()
-            .then(function (response) {
-                $scope.last_name = response.last_name;
-            });
-    };
-
-});*/
 //GOOGLE MAPS
 var map;
 
